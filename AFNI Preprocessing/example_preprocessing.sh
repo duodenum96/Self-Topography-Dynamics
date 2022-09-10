7ba1fa7eb510b0831070cfb4af7d32d4ceb0e27e
duodenum96 <70075569+duodenum96@users.noreply.github.com>
2022-09-10 13:08:39 +0300

    Update example_preprocessing.sh

--- AFNI Preprocessing/example_preprocessing.sh
+++ AFNI Preprocessing/example_preprocessing.sh
@@ -6,7 +6,26 @@ subjpath=/example_subjpath
 anatpath=$subjpath/*t1*
 subj=sub_x
 run=rest.nii # Can also be task etc.
+fspath=$subjpath/fs # Path to output freesurfer
 
+# Run Freesurfer
+
+cd $anatpath
+
+recon-all \
+-all \
+-3T \
+-sd $fspath \
+-subjid $subj \
+-i *t1*.nii \
+-parallel \
+-openmp 16
+
+# AFNI-ize
+
+cd $fspath/$subj/surf
+@SUMA_Make_Spec_FS -sid $subj -NIFTI
+sumapath=$fspath/$subj/surf/SUMA
 
 # Run @SSwarper for skullstripping + Nonlinear alignment
 cd $anatpath
@@ -19,36 +38,43 @@ cd $anatpath
 
 
 # Run afni_proc
-afni_proc.py -subj_id $subj \
--script proc.$subj \
--scr_overwrite \
--out_dir $subj \
+afni_proc.py \
+-subj_id $subj \
 -blocks despike tshift align tlrc volreg mask blur scale regress \
--copy_anat $subjpath/anat_warped/anatSS.$subj.nii \
+-copy_anat $anatpath/anat_warped/anatSS.$subj.nii \
 -anat_has_skull no \
--dsets \
-$run \
+-scr_overwrite \
+-out_dir ${subj}_out \
+-script proc_${subj} \
+-anat_follower anat_w_skull anat $anatpath/anat_warped/anatU.$subj.nii \
+-anat_follower_ROI aaseg anat $sumapath/aparc.a2009s+aseg.nii.gz \
+-anat_follower_ROI aeseg epi $sumapath/aparc.a2009s+aseg.nii.gz \
+-anat_follower_ROI fsvent epi $sumapath/fs_ap_latvent.nii.gz \
+-anat_follower_ROI fswm epi $sumapath/fs_ap_wm.nii.gz \
+-anat_follower_erode fsvent fswm \
+-dsets $subjpath/$run \
 -tcat_remove_first_trs 5 \
 -align_opts_aea -cost lpc+ZZ \
--volreg_align_to MIN_OUTLIER \
--volreg_align_e2a \
--volreg_tlrc_warp -tlrc_base $tpath/$btemplate \
+-tlrc_base $tpath/$btemplate \
 -tlrc_NL_warp \
 -tlrc_NL_warped_dsets \
-$subjpath/anat_warped/anatQQ.$subj.nii \
-$subjpath/anat_warped/anatQQ.$subj.aff12.1D \
-$subjpath/anat_warped/anatQQ.${subj}_WARP.nii \
--volreg_warp_dxyz 3 \
--mask_apply group \
+$anatpath/anat_warped/anatQQ.$subj.nii* \
+$anatpath/anat_warped/anatQQ.$subj.aff12.1D \
+$anatpath/anat_warped/anatQQ.${subj}_WARP.nii* \
+-volreg_align_to MIN_OUTLIER \
 -volreg_post_vr_allin yes \
--mask_segment_anat yes \
 -volreg_pvra_base_index MIN_OUTLIER \
+-volreg_align_e2a \
+-volreg_tlrc_warp \
 -mask_epi_anat yes \
 -mask_apply group \
--mask_segment_erode yes \
+-mask_segment_anat yes \
 -regress_motion_per_run \
+-regress_ROI_PC fsvent 3 \
+-regress_ROI_PC fswm 3 \
+-regress_ROI_PC_per_run fsvent fswm \
 -regress_anaticor_fast \
--regress_ROI WMe CSFe \
+-regress_anaticor_label fswm \
 -regress_apply_mot_types demean deriv \
 -regress_est_blur_epits \
 -regress_est_blur_errts \
@@ -56,8 +82,6 @@ $subjpath/anat_warped/anatQQ.${subj}_WARP.nii \
 -regress_bandpass 0.01 0.2 \
 -volreg_warp_dxyz 3 \
 -regress_censor_motion 0.2 \
--regress_censor_outliers 0.05 \
--html_review_style pythonic
-done
+-regress_censor_outliers 0.05
 
 # After preprocessing is done, interpolate censored time points. See an example in interpolate.m and example_interpolation.m
